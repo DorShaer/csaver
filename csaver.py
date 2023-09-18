38900f3b69e6e10d7c2e881224bfbdd87dc5d8da
Dor Shaer <85877103+DorShaer@users.noreply.github.com>
2023-09-18 11:48:54 +0300

    Update csaver.py

--- csaver.py
+++ csaver.py
@@ -4,17 +4,19 @@ from typing import Dict, List
 import aiohttp
 import argparse
 from termcolor import colored
-from aiohttp.client_exceptions import ClientOSError
+import chardet
+
 
 class Crawler:
-    def __init__(self, interesting_paths: list[str], har_file: str):
+    def __init__(self, base_url: str, interesting_paths: list[str], har_file: str):
+        self.base_url = base_url
         self.interesting_paths = interesting_paths
         self.har_file = har_file
         self.session = None
         self.auth_headers = {}
 
     async def get_headers_and_cookies_from_har(self) -> tuple[Dict[str, str], Dict[str, str]]:
-        with open(self.har_file, 'r') as f:
+        with open(self.har_file, 'rb') as f:
             har_data = json.load(f)
 
         headers = {}
@@ -27,76 +29,65 @@ class Crawler:
                 cookies[cookie['name']] = cookie['value']
         return headers, cookies
 
+
     async def extract_links_from_har(self):
         links = []
-        with open(self.har_file, 'r') as f:
-            har_data = json.load(f)
-            domains = set()
-            for entry in har_data['log']['entries']:
-                request = entry['request']
-                domain = request['url'].split("//")[-1].split("/")[0]
-                domains.add(domain)
-            print(colored("\nDetected domains while parsing the file: ", 'blue', attrs=['bold']))
-            domain_mapping = {}
-            for idx, domain in enumerate(domains):
-                print(f"{idx + 1}. {domain}")
-                domain_mapping[idx + 1] = domain
-            domains_to_test = input(colored("Which domains do you want to include in the test? (comma separated numbers): ",'yellow', attrs=['bold'])).lower()
-            domains_to_test = [int(x.strip()) for x in domains_to_test.split(',')]
-            for idx in domains_to_test:
-                domain = domain_mapping[idx]
-                for entry in har_data['log']['entries']:
-                    request = entry['request']
-                    if domain in request['url']:
-                        post_data = request['postData']['text'] if request['method'] == 'POST' and 'postData' in request else None  # Extract POST data if available
-                        request_headers = {header['name']: header['value'] for header in request['headers']}  # Extract request headers
-                        links.append((request['url'], request['method'], request_headers, post_data))  # Store the link, request method, request headers, and POST data as a tuple
-
+        with open(self.har_file, 'rb') as f:
+            data = f.read()
+            encoding = chardet.detect(data)['encoding']
+            self.har_data = json.loads(data.decode(encoding))
+        for entry in self.har_data['log']['entries']:
+            request = entry['request']
+            if request['url'].startswith(self.base_url):
+                for interesting_path in self.interesting_paths:
+                    if interesting_path in request['url']:
+                        links.append(request['url'])
+                        break
         return links
-    async def process_link(self, link: str, method: str, original_headers: dict, auth_headers: dict, post_data: str = None):
-        try:
-            async with self.session.request(method, link, data=post_data, allow_redirects=False) as resp_without_auth:
-                if resp_without_auth.status != 200:
-                    response_text = await resp_without_auth.text()
-                    response_headers = resp_without_auth.headers
-                    status_code_when_unauthorized = resp_without_auth.status
-                    print(colored('Protected resource found: ', 'green', attrs=['bold']) + colored(f'{method} {link}', 'magenta', attrs=['bold']))
-                    print(colored('Original Request Headers: ', 'yellow', attrs=['bold']) + colored(f'{original_headers}', 'cyan', attrs=['bold']))
-                    if method == 'POST' and post_data:
-                        print(colored('Original POST data: ', 'yellow', attrs=['bold']) + colored(f'{post_data}', 'cyan', attrs=['bold']))
-                    print(colored('Info: ', 'blue', attrs=['bold']) + f'Use status code {status_code_when_unauthorized} as the reauthentication trigger\n')
-        except ClientOSError as e:
-            print(colored(f'Error while processing {method} {link}: {e}', 'red', attrs=['bold']))
 
 
-    async def make_request(self, auth_headers: dict):
+    async def process_link(self, link: str, headers: dict):
+        #print(f'Making request to: {link}')
+        async with self.session.get(link, allow_redirects=False) as resp_without_auth:
+            # if resp_without_auth.status == 200:
+            #     print(f'Request to {link} returned {resp_without_auth.status}')
+            # else:
+            #     print(f'Request to {link} failed with status code: {resp_without_auth.status}')
+            if resp_without_auth.status != 200:
+                print(colored(f'Protected resource found: {link}', 'green', attrs=['bold']))
+                # print(f'Headers: {headers}')
+                # print(f'Cookies: {resp_without_auth.cookies}')
+
+    async def make_request(self, headers: dict):
         tasks = []
-        for link, method, original_headers, post_data in self.links:
-            tasks.append(asyncio.create_task(self.process_link(link, method, original_headers, auth_headers, post_data)))
+        for link in self.links:
+            tasks.append(asyncio.create_task(self.process_link(link, headers)))
         await asyncio.gather(*tasks)
 
+    
     async def run(self):
         self.session = aiohttp.ClientSession()
         links = await self.extract_links_from_har()
         self.links = links
         if not links:
-            print(colored('No links found, try to look for any subdomain or api endpoint.', 'red', attrs=['bold']))
-            await self.session.close()
+            print(colored('No links found.' 'red', attrs=['bold']))
             return
-        headers, cookies = await self.get_headers_and_cookies_from_har()
-        if not headers:
-            print(colored('No headers found', 'red', attrs=['bold']))
-            await self.session.close()
+        auth_headers = await self.get_headers_and_cookies_from_har()
+        if not auth_headers:
+            print(colored('No headers found.' 'red', attrs=['bold']))
             return   
-        await self.make_request(headers)
+        await self.make_request(auth_headers)
         await self.session.close()
-
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("--base-url", required=True, 
+                        help="The base URL of the application")
     parser.add_argument("--har-file", required=True, 
                         help="The har file to extract URLs and auth headers from")
     args = parser.parse_args()
+    base_url = args.base_url
     interesting_paths = "/"
     har_file = args.har_file
-    crawler = Crawler(interesting_paths, har_file)
+    crawler = Crawler(base_url, interesting_paths, har_file)
     asyncio.run(crawler.run())
